90f978a3cf53c2d468747fb994ccba6305b34fab
David Little <littledavid175@gmail.com>
2019-06-19 01:48:57 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -5,3 +5,25 @@ $(document).ready(function(){
 // call functions here
 
 });
+
+function getIt(){
+  document.addEventListener('click',function() {
+    alert('Hey!')
+})}
+function frameIt(){
+   $('img').addClass('tasty')
+}
+function pressIt(){
+  document.addEventListener('keydown',function(key){
+    if(key.which==71){
+    alert('G key was pressed');
+    }
+  })
+}
+function submitIt(){
+  document.addEventListener('submit',function(){
+  if ($( "input:first" ).val() === "correct") {
+    alert('Your form is going to be submitted now.');
+  }
+  })
+}

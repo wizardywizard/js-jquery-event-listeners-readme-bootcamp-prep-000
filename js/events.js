a3052d58f7ad172ead12b4bd604e4bc275c9c121
David Little <littledavid175@gmail.com>
2019-06-20 01:39:36 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -26,6 +26,7 @@ function submitIt(){
   $('form').on('submit',function(){
   if ($( "input:first" ).val() === "correct") {
     alert('Your form is going to be submitted now.');
+    return;
   }
   })
 }

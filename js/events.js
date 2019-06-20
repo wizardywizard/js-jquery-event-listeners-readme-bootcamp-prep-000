710de93cbfccd4a31e75191c1c19bdd1cf38b038
David Little <littledavid175@gmail.com>
2019-06-20 00:55:53 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -15,22 +15,15 @@ function frameIt(){
 }
 function pressIt(){
   document.addEventListener('keydown',function(key){
-<<<<<<< HEAD
     if(key.which==71){
     alert('G key was pressed');
     }
-=======
-    alert('G key was pressed')
->>>>>>> 4a89829b2c09644eadcc78e2b745b1695a95b5c4
   })
 }
 function submitIt(){
   document.addEventListener('submit',function(){
   if ($( "input:first" ).val() === "correct") {
-<<<<<<< HEAD
     alert('Your form is going to be submitted now.');
-=======
-    alert('your form is going to be submitted now.');
   }
   })
 }
